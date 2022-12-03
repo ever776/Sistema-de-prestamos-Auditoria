f4406755063db5de01a3b5e2ec88fac8e22c03fb
Ever Quispe <derilrever776@gmail.com>
2022-12-03 10:51:38 -0400

    Exm Final Auditoria

---
+++ app/db_wallet.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+use OwenIt\Auditing\Contracts\Auditable;
+
+class db_wallet extends Model implements Auditable
+{
+    use \OwenIt\Auditing\Auditable;
+    protected $table = 'wallet';
+    public $timestamps = false;
+}
